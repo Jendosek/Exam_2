90977d58421fd629c6ec748175af8cc5596c9ee3
student <student@PC-T-02>
2024-03-28 20:24:57 +0200

    Add project files.

---
+++ Exam.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+using namespace std;
+
+
+
+
+int main() {
+
+	cout << endl;
+	return 0;
+}
+
+//class Car {
+//private:
+//	string marka = "BMW";
+//	string model = "x5";
+//	string year = "2021";
+//	string volume = "6.3";
+//
+//public:
+//	void GetMarka() {
+//		cout << "Marka: " << marka << endl;
+//	}
+//	void GetModel() {
+//		cout << "Model: " << model << endl;
+//	}
+//	void GetYear() {
+//		cout << "YeaR: " << year << endl;
+//	}
+//	void GetVolume() {
+//		cout << "Volume: " << volume << endl;
+//	}
+//
+//};
+//
+//
+//int main() {
+//	Car obj;
+//	obj.GetMarka();
+//	obj.GetModel();
+//	obj.GetYear();
+//	obj.GetVolume();
+//}
+
+
+//class Rectangle {
+//private:
+//	int a, b;
+//public:
+//	Rectangle() {
+//		cout << "Enter 1 side: ";
+//		cin >> a;
+//		cout << "Enter 2 side: ";
+//		cin >> b;
+//	}
+//	void GetS() {
+//		int S = a * b;
+//		cout << "S: " << S << endl;
+//	}
+//	void GetP() {
+//		int P = 2 * (a + b);
+//		cout << "P: " << P << endl;
+//	}
+//};
+//
+//
+//int main() {
+//	Rectangle obj;
+//	obj.GetS();
+//	obj.GetP();
+//}
+
+
+//int main()
+//{
+//    string line;
+//    string name;
+//    int age;
+//
+//    cout << "Enter your name: ";
+//    cin >> name;
+//    cout << "Welcome in my app, " << name << endl;
+//    cout << "Enter your age: ";
+//    cin >> age;
+//    if (age <= 17) {
+//        cout << "You are not of legal age!!!" << endl;
+//    }
+//    else {
+//        cout << "You are of legal age" << endl;
+//    }
+//    cout << "Enter line: ";
+//    cin >> line;
+//    for (int i = line.length() - 1; i >= 0; i--)
+//    {
+//        cout << line[i];
+//    }
+//
+//    return 0;
+//}
+
